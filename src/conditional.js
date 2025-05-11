aaab2eb05178eca7f27fa5b45a7c764965749522
Miriam Bissoli <126024992+miriambixso02@users.noreply.github.com>
2025-05-11 16:30:04 +0200

    First styling of the page

--- src/conditional.js
+++ src/conditional.js
@@ -1,56 +1,47 @@
-const REPORTS_BASEPATH = '../public/resources/documents/'
-
-const reports = [];
-for (let j = 0; j < 9; j++) {
-  reports.push()
-}
+const REPORTS_BASEPATH = './resources/documents/'
 
 window.onload = function () {
   // Get the the input when the user submits their answer
-  document.getElementById("submit").addEventListener("click", function () {
+  document.getElementById("solutionForm").addEventListener("submit", function (e) {
+    e.preventDefault(); // prevent page reload
     GetSolution();
   });
 
   // Get the modal
   let modal = document.getElementById("report");
+  let content = document.getElementById("content");
   // Get the element that closes the modal
   let close = document.getElementsByClassName("close")[0];
+
   // When the user clicks on (x), close the modal
   close.onclick = function () {
     modal.style.display = "none";
-  }
-  // When the user clicks anywhere outside of the modal, close it
-  // TODO: see if necessary because we might make the documents interactive
-  /* window.onclick = function (event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  } */
+    content.innerHTML = '';
+  };
 
-  // convert the input to lower case
+  // Convert the input to lower case
   function GetSolution() {
     let input = document.getElementById("solution").value.trim().toLowerCase();
     CheckSolution(input);
   }
   // Check if the input is a solution
   function CheckSolution(solution) {
-    const image = document.getElementById("reportImage");
-
     let message = "";
-    let imgSrc = "";
 
+    // Show modal based on user input
     switch (solution) {
-      case "eye":
-        modal.style.display = "flex";
-        imgSrc = `${REPORTS_BASEPATH}report1.png`;
+      case "aftermath":
+        const iframe = document.createElement('iframe');
+        iframe.src = `${REPORTS_BASEPATH}the_columbia_post.pdf`;
+        iframe.style.width = "100%";
+        iframe.style.height = "90vh";
+        iframe.style.border = "none";
+        content.appendChild(iframe);
+
+        modal.style.display = "block";
         break;
       case "rat":
-        modal.style.display = "flex";
-        imgSrc = `${REPORTS_BASEPATH}report2.png`;
-        break;
-      case "king":
-        modal.style.display = "flex";
-        imgSrc = `${REPORTS_BASEPATH}report3.png`;
+
         break;
       case "betrayal":
         message = "THIS CODE CAN NOT BE USED HERE";
@@ -59,7 +50,6 @@ window.onload = function () {
         message = "Only those Chosen by The Authority God can grasp the true essence of the world!";
         break;
     }
-    document.getElementById("text").innerText = message;
-    image.src = imgSrc;
+    document.getElementById("message").innerText = message;
   }
 }
