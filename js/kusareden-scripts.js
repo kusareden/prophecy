7972ace296566bd01f7ddce6c12f85d8f8ead2ec
Miriam Bissoli <126024992+miriambixso02@users.noreply.github.com>
2025-05-18 18:23:55 +0200

    solution encryption

--- js/kusareden-scripts.js
+++ js/kusareden-scripts.js
@@ -25,12 +25,15 @@ window.onload = function () {
     CheckSolution(input);
   }
   // Check if the input is a solution
-  function CheckSolution(solution) {
+  async function CheckSolution(solution) {
     let message = "";
 
+    const encodedSolution = await digestMessage(solution)
+    console.debug(encodedSolution)
+
     // Show modal based on user input
-    switch (solution) {
-      case "aftermath":
+    switch (encodedSolution) {
+      case "bf648e312779342bb0c687654e34338830c9a6df999f1d64bf3e2237e93e222a":
         const iframeNewspaper = document.createElement('iframe');
         iframeNewspaper.src = `${REPORTS_BASEPATH}the_columbia_post.pdf`;
         iframeNewspaper.style.width = "100%";
@@ -40,7 +43,7 @@ window.onload = function () {
 
         modal.style.display = "block";
         break;
-      case "wanted":
+      case "ca9066873bf335f3c1d9f0f6986266ee50689a816763851d82791d3dbc31cbf5":
         const iframeProfiles = document.createElement('iframe');
         iframeProfiles.src = `${REPORTS_BASEPATH}profiles.pdf`;
         iframeProfiles.style.width = "100%";
@@ -50,8 +53,21 @@ window.onload = function () {
 
         modal.style.display = "block";
         break;
-      case "betrayal":
+      case "a6b39fa8cb7ca966708528463ef77c3db94c52a959c97347f93b4715d6e11470":
+        /*  const iframeProfiles = document.createElement('iframe');
+         iframeProfiles.src = `${REPORTS_BASEPATH}profiles.pdf`;
+         iframeProfiles.style.width = "100%";
+         iframeProfiles.style.height = "90vh";
+         iframeProfiles.style.border = "none";
+         content.appendChild(iframeProfiles); */
+
+        modal.style.display = "block";
+        break;
+      case "eaa22de66e34c450159b024a070c0b6ebf8ad9b50cdec3b803db6df85ac3ddeb":
         message = "THIS CODE CAN NOT BE USED HERE";
+        break;
+      case "1f0fb05f858637c5eba5b004630aab6c6d6634adb58af89ece0435ef1c62704f":
+        message = "YOU HAVE ALREADY USED THIS SOLUTION";
         break;
       default:
         message = "Only those Chosen by The Authority God can grasp the true essence of the world!";
@@ -60,3 +76,20 @@ window.onload = function () {
     document.getElementById("message").innerText = message;
   }
 }
+
+/// ======== DIGEST GENERATOR ======== ///
+
+// https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest#converting_a_digest_to_a_hex_string
+
+async function digestMessage(message) {
+  const msgUint8 = new TextEncoder().encode(message); // encode as (utf-8) Uint8Array
+  const hashBuffer = await window.crypto.subtle.digest("SHA-256", msgUint8); // hash the message
+  const hashArray = Array.from(new Uint8Array(hashBuffer)); // convert buffer to byte array
+  const hashHex = hashArray
+    .map((b) => b.toString(16).padStart(2, "0"))
+    .join(""); // convert bytes to hex string
+  return hashHex;
+}
+
+// TO GENERATE A NEW MESSAGE
+// await digestMessage(<NEW MESSAGE>)
